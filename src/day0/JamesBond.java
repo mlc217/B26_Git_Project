68217821cc39616e0e4146a16dcb951805ba6d7a
mlc217 <mlc217@yahoo.com>
2022-03-26 16:34:07 -0400

    Updated JamesBond file

--- src/day0/JamesBond.java
+++ src/day0/JamesBond.java
@@ -1,7 +1,8 @@
-package day0
+package day0;
 
-public class JamesBond
-  
-  public static void main (String []args){
-  System.out.println("James Bond hacked the project")
+public class JamesBond {
+
+  public static void main(String[] args) {
+    System.out.println("James Bond hacked the project");
   }
+}
